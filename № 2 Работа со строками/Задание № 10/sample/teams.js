ad0c712a349f4676ddb40adb5bdf776ea32a7a43
rgrrg <yrfgerg@example.com>
2025-04-27 12:24:46 +0600

    first commit

---
+++ № 2 Работа со строками/Задание № 10/sample/teams.js
@@ -0,0 +1,9 @@
+let teams = 
+`
+1;Aerosmith;aerosmith;США;1970;хард-рок;assets/teams/aerosmith.jpg
+2;Pink Floyd;pink-floyd;Великобритания;1965;психоделический-рок;assets/teams/pink-floyd.jpg
+3;The Beatles;the-beatles;Великобритания;1960;рок-н-ролл;assets/teams/beatles.jpg
+4;AC/DC;ac-dc;Австралия;1973;хард-блюз-рок;assets/teams/acdc.jpg
+5;Scorpions;scorpions;ФРГ;1965;хард-рок;assets/teams/scorpions.jpg
+6;Ленинград;ленинград;Россия;1997;ска, фолк, панк;assets/teams/leningrad.jpg
+`;
